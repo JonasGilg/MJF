f2f9c617ab68d08c4486b6b30b2634a8c845ad62
jonas <jonas.gilg@stud.hs-hannover.de>
2017-11-15 15:59:53 +0100

    lol we nubs

--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -7,16 +7,15 @@ import org.apache.jena.rdf.model.ModelFactory
 
 fun main(args: Array<String>) {
 	val model = ModelFactory.createDefaultModel()
-	model.read("file:src/main/resources/mjfOntology.ttl", "TURTLE")
+	model.read("file:C:\\Users\\jonas\\IntelliJProjects\\MJF\\kotlin\\src\\main\\resources\\mjfOntology.ttl", "TURTLE")
 
 	val query = QueryFactory.create(
 			"PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
 			"PREFIX mjf: <http://michajonasfood.com/>\n" +
 			"\n" +
-			"SELECT ?name\n" +
+			"SELECT ?x\n" +
 			"WHERE {\n" +
-			"    ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> mjf:food ;\n" +
-			"    foaf:name ?name .\n" +
+			"    ?x ?z mjf:food .\n" +
 			"}")
 
 	val result = QueryExecutionFactory.create(query, model)
