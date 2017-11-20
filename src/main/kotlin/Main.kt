dac0b3592a7f7c8242be36c61785fe1da2d121a2
jonas <jonas.gilg@stud.hs-hannover.de>
2017-11-20 23:40:27 +0100

    fixed gradle shiz

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,17 @@
-
 import org.apache.jena.query.QueryExecutionFactory
 import org.apache.jena.query.QueryFactory
 import org.apache.jena.query.ResultSetFormatter
 import org.apache.jena.rdf.model.ModelFactory
 
 
-class Main {
-    companion object {
-        @JvmStatic fun main(args: Array<String>) {
-            val model = ModelFactory.createDefaultModel()
-            model.read(Queries.javaClass.getResource("mjfOntology.ttl").file, "TURTLE")
+fun main(args: Array<String>) {
+	val model = ModelFactory.createDefaultModel()
+	model.read(Queries.javaClass.getResource("mjfOntology.ttl").file, "TURTLE")
 
-            val query = QueryFactory.create(Queries.fridgeContents)
+	val query = QueryFactory.create(Queries.fridgeContents)
 
-            val result = QueryExecutionFactory.create(query, model)
-            val results = result.execSelect()
+	val result = QueryExecutionFactory.create(query, model)
+	val results = result.execSelect()
 
-            ResultSetFormatter.out(System.out, results, query)
-        }
-    }
-
+	ResultSetFormatter.out(System.out, results, query)
 }
